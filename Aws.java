9845af8a4c0a61757a93bc0d14de19031070f228
Nathan Nigussie Tolosa <nathantolosa11@gmail.com>
2025-01-23 12:43:33 +0200

    Update Aws.java

--- Aws.java
+++ Aws.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        // Prints "Hello, AWS learner" in the terminal i can write anything
+        // Prints "Hello, AWS learner" in th the terminal i can write anything
         System.out.println("Hello nathan AWS learner");
     }
 }
